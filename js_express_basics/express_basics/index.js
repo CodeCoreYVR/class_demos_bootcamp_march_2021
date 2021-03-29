b1231bf6633aaaca046f0a5c5265eadc099ccce3
Naveed Raza <naveedraza@gmail.com>
2021-03-29 11:16:03 -0700

    setting up view engine

---
+++ js_express_basics/express_basics/index.js
@@ -0,0 +1,43 @@
+const express=require('express'); //returns a function that returns instance of an express app.
+const logger=require('morgan');
+
+const app=express();// The app variable reference in index.js is an object with bunch of methods to cofigure our app, some common method that we use are :
+
+
+// app.use() to initialize middleware
+// app.engine() to set view engine of express
+// app.listen() to start the express server
+
+//  CRUD METHODS inside app
+/*
+app.get
+app.post
+app.patch
+app.delete
+*/
+app.use(logger('dev'));
+
+app.set('view engine','ejs');
+app.set('views','views')
+
+app.get('/',(request,response)=>{
+    //your code here
+    response.send('<h1>Welcome</h1>');
+});
+app.get('/hello_world',(request,response)=>{
+    //your code here
+    response.send('<h1>Hello World</h1><p>Welcome to the coding</p>');
+});
+app.get('/survey',(request,response)=>{
+    //your code here
+    response.send('<h1>My Survey Page</h1>');
+});
+
+
+
+const PORT=3000;
+const DOMAIN='localhost'; // loopback address: 127.0.0.1
+
+app.listen(PORT,DOMAIN,()=>{
+    console.log(`🖥 Server is listening on http://${DOMAIN}:${PORT}`)
+})
