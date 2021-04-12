b080204e1e2bb1cca3d04f64a802bbdd08f7da6b
Naveed Raza <naveedraza@gmail.com>
2021-04-12 11:37:24 -0700

    attribute accessors

--- ruby_oop/car.rb
+++ ruby_oop/car.rb
@@ -8,7 +8,25 @@ class Car
     # def park
     #     puts "ZZZZzzzzz"
     # end
+    def initialize(model, type, capacity)
+        @model = model 
+        @type = type 
+        @capacity = capacity
+          puts "Creating a beautifull car"
+      end
+      def capacity
+        @capacity
+    end
+    # def capacity= (capacity)
+    #     @capacity = capacity
+    # end
+    attr_accessor :model, :type, :capacity
+    def self.max_speed
+        'max speed of the cars produced by this factory is 200'
+    end
+
     def drive
+        ignite_engine()
         puts 'driving car'
       end
       def stop
@@ -16,6 +34,9 @@ class Car
       end
       def park
         puts 'parking car'
+      end
+      def details
+        puts "model is #{@model} - type is #{@type} - capacity is #{@capacity}"
       end
       private
     #   def pump_gas
@@ -25,11 +46,31 @@ class Car
     #     puts "🚗 💥 🔥🔥🔥"
     #   end
     private
-    def gas
+    def pump_gas
         puts "pumping gas"
     end
-    def engine
+    def ignite_engine
         puts "igniting engine"
     end
   
 end
+my_car = Car.new('Mazda','Sedan', 4)
+my_car.capacity=5
+puts my_car.capacity
+my_car.drive
+my_car.stop
+my_car.park
+my_car.details
+
+blue_car = Car.new('Mazda','Sedan', 4)
+red_car = Car.new('Mazda','Sedan', 4)
+blue_car.details
+red_car.details
+blue_car.drive
+red_car.drive
+blue_car.stop
+red_car.stop
+blue_car.park
+red_car.park
+
+puts Car.max_speed
