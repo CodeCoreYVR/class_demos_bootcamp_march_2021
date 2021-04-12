bfc67569f5ce3ba968c0cd337c59863184738ecb
Naveed Raza <naveedraza@gmail.com>
2021-04-12 09:52:17 -0700

    Method and Methods Visibility: Private Public

--- ruby_oop/car.rb
+++ ruby_oop/car.rb
@@ -1,3 +1,35 @@
 class Car
+    # def drive
+    #     puts 'Vroom Vroom'
+    # end
+    # def stop
+    #     puts "Screech"
+    # end
+    # def park
+    #     puts "ZZZZzzzzz"
+    # end
+    def drive
+        puts 'driving car'
+      end
+      def stop
+        puts 'stopping car'
+      end
+      def park
+        puts 'parking car'
+      end
+      private
+    #   def pump_gas
+    #     puts 'glug glug⛽️'
+    #   end
+    #   def ignite_engine
+    #     puts "🚗 💥 🔥🔥🔥"
+    #   end
+    private
+    def gas
+        puts "pumping gas"
+    end
+    def engine
+        puts "igniting engine"
+    end
   
 end
--- ruby_oop/cookie.rb
+++ ruby_oop/cookie.rb
@@ -2,5 +2,23 @@
 # You need to use keyword 'class' and the 'end'
 # Then comes a class name and the conevention for writing a class name is CamelCase
 class Cookie
+    def bake_n_eat
+        puts bake()
+        puts eat()
+    end
+    
+    def eat
+        'Nom. Nom. Nom!'
+    end
+    private
+    def bake
+        'baking the cookie'
+    end
 
 end
+
+my_cookie = Cookie.new
+
+# puts my_cookie.class
+my_cookie.bake_n_eat
+puts my_cookie.eat
