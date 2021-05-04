0a44ae1edcf88bb3148974ffa82f8bb13578aa64
Naveed Raza <naveedraza@gmail.com>
2021-05-04 12:03:22 -0700

    js-DOM

---
+++ rails_basics/cowsay.rb
@@ -0,0 +1,12 @@
+require 'cowsay'
+
+puts Cowsay.say('Hello World!',:cheese)
+
+puts Cowsay.random_character.say('Hello!')
+
+p Cowsay.character_classes
+
+
+# # To create file 
+# $>touch .railsrc
+# >code ~/.railsrc
